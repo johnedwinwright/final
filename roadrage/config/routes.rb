766218047751d7791bf871ef806c52c0d61cc9bb
John Wright <JohnedwinWright@gmail.com>
2015-12-07 20:16:25 -0500

    added pretties

--- roadrage/config/routes.rb
+++ roadrage/config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   resources :vehicles
   resources :rages
   devise_for :users
-  root to: "home#index"
+  root to: "vehicles#index"
 
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
