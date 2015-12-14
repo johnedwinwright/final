d8f0347042f9ec53f9a99fb68d38eb3410ee66e3
John Wright <JohnedwinWright@gmail.com>
2015-12-14 12:29:00 -0500

    added db fields to user

--- roadrage/db/schema.rb
+++ roadrage/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208003201) do
+ActiveRecord::Schema.define(version: 20151214172502) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -48,6 +48,9 @@ ActiveRecord::Schema.define(version: 20151208003201) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "plate_number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
